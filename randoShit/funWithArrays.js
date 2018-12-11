05fbd1bccfe387d71edf5bc5d876225d7a651e4c
Googolbyte <irving.severino@gmail.com>
2018-12-11 11:50:22 -0800

    Start of Minesweeper Part 2

---
+++ randoShit/funWithArrays.js
@@ -0,0 +1,6 @@
+const countries = ['Japan', 'Denmark', 'Mexico', 'Morocco'];
+countries.shift();
+console.log(countries);
+countries.unshift("Murica");
+countries =  ['England', 'Mozambique', 'Cambodia', 'Peru'];
+console.log(countries);
